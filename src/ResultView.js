c87847687c411b22d1cea4c993ef3e05e9f36435
Ankur Mittal <mittankur1999@gmail.com>
2021-03-14 05:05:20 +0530

    add socket.io-client

--- src/ResultView.js
+++ src/ResultView.js
@@ -1,7 +1,19 @@
 import React from "react";
 import {Paper} from "@material-ui/core";
 
+import {io} from "socket.io-client";
+
 function ResultView() {
+    const socket = io("http://localhost:5000/results");
+    socket.on("connect", () => {
+        socket.emit("images", {data: "images/jpeg, base64, abcdefghijklmnopqrstuvwxyz"});
+        socket.on("processed_images", (data) => {
+            console.log(data);
+            // do something with processed images
+            socket.disconnect();
+        });
+    });
+
     return (
         <React.Fragment>
             <Paper variant="outlined">
